e7aa53cb6d743abff2e92220b9aff8af8250c8de
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-08-13 15:19:30 -0500

    working on syllabizer algorithm

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import _ from 'underscore';
 
 import store from './store';
 import settings from './settings';
@@ -15,3 +16,58 @@ $(document).ajaxSend(function(e, xhr, jqueryAjax){
 });
 
 ReactDOM.render(router, document.getElementById('container'));
+
+let string = "Wow what a cooel maessage";
+
+console.log(syllablizer(string));
+
+function syllablizer (string) {
+  string = string.toLowerCase();
+  let arr = string.split(' ');
+  arr = arr.map(function(word){
+    return word + ' ';
+  })
+  arr = arr.map(function(word){
+    let vowelIndex = [];
+    let wordArr = word.split('');
+    wordArr.forEach(function(letter, i){
+      if (letter==='a' || letter==='e' || letter==='i' || letter==='o' || letter === 'u') {
+        // console.log('v');
+        vowelIndex.push(i)
+      }
+    });
+    let vowelClusters = vowelIndex.map(function(vowelSpot, i) {
+      return wordArr[vowelSpot];
+    });
+    for (var i = 0; i < vowelIndex.length; i++) {
+      if(vowelIndex[i+1]-vowelIndex[i] === 1) {
+        if(vowelIndex[i+2]-vowelIndex[i+1] === 1) {
+          vowelClusters[i] = (word[vowelIndex[i]]+word[vowelIndex[i+1]]+word[vowelIndex[i+2]]);
+          vowelClusters[i+1] = '';
+          vowelClusters[i+2] = '';
+          i+=2;
+        } else {
+          vowelClusters[i] = (word[vowelIndex[i]]+word[vowelIndex[i+1]]);
+          vowelClusters[i+1] = '';
+          i++;
+        }
+      }
+    }
+    vowelClusters = _.compact(vowelClusters);
+    if (vowelClusters.length <= 1) {
+      return word
+    } else {
+      return vowelClusters;
+      // for (var i = 0; i <= vowelIndex[0]; i++) {
+      //   syllableArr.push(word[i]);
+      // }
+      // if (vowelIndex[1]-vowelIndex[0] === 1) {
+      //   syllableArr.push(word[vowelIndex[1]]);
+      // }
+      // if
+      // syllableArr = syllableArr.join('');
+      // return syllableArr;
+    }
+  });
+  return _.flatten(arr);
+}
